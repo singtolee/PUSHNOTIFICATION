7510f1b488548fa1a9994e88fd7f3a61a9abb628
QIANG LI <qiangli@QIANGs-Mac-mini.local>
2018-12-19 17:58:21 +0700

    push notis

---
+++ functions/src/index.ts
@@ -0,0 +1,24 @@
+import * as functions from 'firebase-functions';
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.pushNotification = functions.firestore.document('MYORDERS/{orderId}').onUpdate((change,context)=>{
+    const order = change.after.data();
+    const oid = context.params.orderId;
+    const uid = order.uid;
+    return admin.firestore().collection("USERS").doc(uid).get().then(queryResult=>{
+        const token = queryResult.data().deviceToken;
+        const payload = {
+            notification:{
+                title: "WHY ERROR",
+                body:"WHO MAKE ERROR"
+            },
+            token:token
+        };
+        return admin.messaging().send(payload).then((resp)=>{
+            console.log("SUCCESS OF : ",resp);
+        }).cathe((error)=>{
+            console.log("ERROR OF : ",error);
+        })
+    });
+})
